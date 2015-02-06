cb71debf8a88074bbb9850a43b6538190a9db326
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2015-02-06 11:48:49 -0300

    EmptyResponseRequest tests

---
+++ volley-requests/src/test/java/com/monits/volleyrequests/network/request/EmptyResponseRequestTest.java
@@ -0,0 +1,43 @@
+package com.monits.volleyrequests.network.request;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.Collections;
+
+import static junit.framework.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+
+@RunWith(RobolectricTestRunner.class)
+public class EmptyResponseRequestTest {
+
+	private EmptyResponseRequest request;
+
+	@Before
+	public void setUp() {
+		request = new EmptyResponseRequest(Request.Method.GET, "http://www.google.com/",
+				new DummyListener<Void>(), null, null);
+	}
+
+	@Test
+	public void testParseNetworkResponse() {
+		final NetworkResponse networkResponse = new NetworkResponse(null);
+		final Response<Void> response = request.parseNetworkResponse(networkResponse);
+
+		assertNull(response.result);
+	}
+
+	// Dummy empty implementation of listener class.
+	private static class DummyListener<T> implements Response.Listener<T> {
+		@Override
+		public void onResponse(final T objects) {
+			// Dummy method does nothing
+		}
+	}
+}
